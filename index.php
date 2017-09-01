605a99fde96af5b478cfbf98a94a361cc3d28b92
almina-mac <alm.0554.mina@gmail.com>
2017-09-01 18:15:10 +0900

    fix index.php: city name branch

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ foreach ($events as $event) {
   }
 
   error_log(file_get_contents('php://input'));
-  error_log('location'.var_export($location, true));
+  error_log('location: '.var_export($location, true));
 
   // get user input
   // $location = $event->getText();
@@ -101,7 +101,7 @@ foreach ($events as $event) {
   // extract city and compare with user input
   foreach ($crawler->filter('channel ldWeather|source pref city') as $city) {
     // if match city name, get location ID
-    if (strpos($city->getAttribute('title'), $location) !== false) {
+    if (strpos($city->getAttribute('title'), $location) || strpos($city->getAttribute('title').'市', $location)) {
       $locationId = $city->getAttribute('id');
       break;
     }
@@ -120,7 +120,7 @@ foreach ($events as $event) {
     // extract prefacture and compare with user input
     foreach ($crawler->filter('channel ldWeather|source pref') as $pref) {
       // if match prefacture, get suggested city
-      if (strpos($pref->getAttribute('title'), $location) !== false) {
+      if (strpos($pref->getAttribute('title'), $location)) {
         foreach ($pref->childNodes as $child) {
           if ($child instanceof DOMElement && $child->nodeName == 'city') {
             array_push($suggestArray, $child->getAttribute('title'));
